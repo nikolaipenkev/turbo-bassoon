845bc55fa9881c4315908b70a74d3f3690b02460
nikolaipenkev <nikolai.penkev@merkle.com>
2025-09-13 13:28:42 +0200

    Fixed comments

--- clean_dataset.py
+++ clean_dataset.py
@@ -8,10 +8,7 @@ from src.data_correction import correct_dataset
 from src.config import DATASET_PATH
 
 if __name__ == "__main__":
-    # Load the dataset
     df = load_dataset(DATASET_PATH)
-    # Apply data corrections
     df_clean = correct_dataset(df)
-    # Save the cleaned dataset to a new CSV file
     df_clean.to_csv("test_dataset_clean.csv", index=False)
     print("Cleaned dataset saved as test_dataset_clean.csv")
